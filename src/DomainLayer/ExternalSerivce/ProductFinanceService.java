9f21df9eabcd458ac78695089078b3f739ea0294
tamer <tamerna@post.bgu.ac.il>
2020-04-24 18:37:46 +0300

    Added externalFinanceService class for UseCase 7

--- src/DomainLayer/ExternalSerivce/ProductFinanceService.java
+++ src/DomainLayer/ExternalSerivce/ProductFinanceService.java
@@ -1,13 +1,20 @@
 package DomainLayer.ExternalSerivce;
 
 import DomainLayer.DealDetails;
+import DomainLayer.ExternalSerivce.PassiveObjects.ExternalFinanceService;
 
 public class ProductFinanceService implements ExternalService {
 
+    private ExternalFinanceService FinanceService;
 
     @Override
     public boolean connect() {
-        return true;
+        try{
+            this.FinanceService.connect();
+            return true;
+        }catch(Exception e){
+            return false;
+        }
     }
 
 
