b33f568d21df1a340507da4efc1c15d353e6ef55
ahmadf@post.bgu.ac.il <442126af>
2020-05-18 15:27:08 +0300

    Working Singup

--- src/NetworkLayer/passiveObjects/MessageProccess.java
+++ src/NetworkLayer/passiveObjects/MessageProccess.java
@@ -2,18 +2,21 @@ package NetworkLayer.passiveObjects;
 
 import DomainLayer.InternalService.SubscribersManage_Facade;
 import DomainLayer.InternalService.SystemManage_Facade;
+import DomainLayer.Product;
 import DomainLayer.Store.Store;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MessageProccess {
 
-    public static void Login(MessagingProtocol protocol, JSONObject request) throws Exception{
+
+    public static void Login(MessagingProtocol protocol, JSONObject request) throws Exception {
         JSONObject response = new JSONObject();
         response.put("req", request.get("req"));
-        if(SubscribersManage_Facade.login((String)request.get("username"),(String)request.get("password")))
+        if (SubscribersManage_Facade.login((String) request.get("username"), (String) request.get("password")))
             response.put("res", "true");
         else
             response.put("res", "false");
@@ -45,5 +48,84 @@ public class MessageProccess {
 
 
     }
+
+
+    public static void getStore(MessagingProtocol protocol, JSONObject request) throws Exception {
+        List<Store> stores = SystemManage_Facade.getAllStores();
+        String store = request.getString("store");
+
+        for (Store s : stores) {
+            if(s.getName().equals(store)) {
+                JSONObject o = new JSONObject();
+                o.put("name", store);
+                o.put("is_open", s.isIs_open());
+                o.put("req", request.get("req"));
+
+                protocol.send(o);
+
+                return;
+            }
+        }
+
+    }
+
+    public static void Signup(MessagingProtocol protocol, JSONObject request) throws Exception{
+        JSONObject response = new JSONObject();
+        response.put("req", request.get("req"));
+        if(SubscribersManage_Facade.signup((String)request.get("username"),(String)request.get("password")))
+            response.put("res", "true");
+        else
+            response.put("res", "false");
+        response.put("username", request.get("username"));
+        protocol.send(response);
+    }
+
+    public static void searchProductStores(MessagingProtocol protocol, JSONObject request) throws Exception {
+        String name = request.getString("name");
+        HashMap<String, Double> stores_holding_product = SystemManage_Facade.searchProductStores(name);
+
+        List<JSONObject> res = new ArrayList<>();
+
+        for (String store : stores_holding_product.keySet()) {
+            Double price = stores_holding_product.get(store);
+            JSONObject o = new JSONObject();
+            o.put("store", store);
+            o.put("price", price);
+            o.put("product", name);
+            res.add(o);
+        }
+
+        for (JSONObject o : res) {
+            o.put("req", request.get("req"));
+            protocol.send(o);
+        }
+    }
+
+    public static void searchProductsOfStore(MessagingProtocol protocol, JSONObject request) throws Exception {
+        String store = request.getString("store");
+       // List<Product> products = SystemManage_Facade.get_products_of_store(store);
+        String[][] products = SystemManage_Facade.get_products_of_store(store);
+     /*   for(Product p : products){
+            JSONObject o = new JSONObject();
+            o.put("price",p.getPrice());
+            o.put("amount",p.getAmount());
+            o.put("name",p.getName());
+            o.put("store", p.getStore().getName());
+            o.put("req", request.get("req"));
+*/
+
+        for(String[] p : products){
+            JSONObject o = new JSONObject();
+            o.put("name",p[0]);
+            o.put("price",p[1]);
+            o.put("amount",p[2]);
+            o.put("store", p[3]);
+            o.put("req", request.get("req"));
+
+            protocol.send(o);
+        }
+
+    }
+
 
 }
--- src/NetworkLayer/passiveObjects/MessagingProtocol.java
+++ src/NetworkLayer/passiveObjects/MessagingProtocol.java
@@ -11,6 +11,17 @@ public class MessagingProtocol {
     }
 
 
+    /**
+     * process client incoming requests
+     *
+     * {@param msg} would be a JSON string in the following format
+     *      { req:string, ...args:any }
+     *
+     * - req will define which action the
+     *  client is requesting
+     *
+     * - args will hold the action arguments
+     * **/
     public void proccess(String msg){
         //TODO: implement msg protocol
         try {
@@ -23,6 +34,14 @@ public class MessagingProtocol {
                 MessageProccess.Login(this, request);
             }else if(req.equals("get_stores")){
                 MessageProccess.getStores(this, request);
+            }else if(req.equals("get_products")){
+                MessageProccess.searchProductStores(this, request);
+            }else if(req.equals("get_store")){
+                MessageProccess.getStore(this, request);
+            }else if(req.equals("get_product_of_store")){
+                MessageProccess.searchProductsOfStore(this, request);
+            }else if(req.equals("signup")){
+                MessageProccess.Signup(this, request);
             }
         }catch (Exception e){
             System.out.println("Can't process " + msg);
@@ -32,7 +51,7 @@ public class MessagingProtocol {
 
 
     public void send(JSONObject msg){
-        connectionHandler.send(msg.toString());
+        send(msg.toString());
     }
 
     public void send(String msg){
