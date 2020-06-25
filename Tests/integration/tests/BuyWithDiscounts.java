6d1fa5c1a2df2475b7034d92e19cd7a031c1c9bc
Ygrno <yakirgre@post.bgu.ac.il>
2020-06-25 13:48:43 +0300

    Discounts fixes

--- Tests/integration/tests/BuyWithDiscounts.java
+++ Tests/integration/tests/BuyWithDiscounts.java
@@ -66,7 +66,58 @@ public class BuyWithDiscounts {
         subscriberImp.save_products("s2","pc","computers",1);
 
         subscriberImp.buy_products_in_cart("s2","moti","1234567891234567","12/24",123,"123456789","","","","");
+        assertEquals(0, s2.getPurchaseProcesslist().get(1).getShoppingBag().getProducts().get(2).getPrice(), 0.0);
+        assertEquals(0, s2.getPurchaseProcesslist().get(1).getShoppingBag().getProducts().get(5).getPrice(), 0.0);
+        assertEquals(0, s2.getPurchaseProcesslist().get(1).getShoppingBag().getProducts().get(0).getPrice(), 750);
 
+        storeRoleImp.delete_discount("s1","computers","D1");
+        storeRoleImp.delete_discount("s1","computers","D2");
+
+        storeRoleImp.add_conditioned_discount("s1","computers","pc","D1",0.5,12122104,0,100);
+        subscriberImp.save_products("s2","pc","computers",1);
+        subscriberImp.save_products("s2","pc","computers",1);
+
+        subscriberImp.buy_products_in_cart("s2","moti","1234567891234567","12/24",123,"123456789","","","","");
+        assertEquals(1500,s2.getPurchaseProcesslist().get(2).getShoppingBag().getDiscounted_bag_price(), 0.05);
+
+        storeRoleImp.delete_discount("s1","computers","D1");
+
+        storeRoleImp.add_store_product("s1","computers","apple",100,100);
+        storeRoleImp.add_store_product("s1","computers","orange",100,100);
+
+        storeRoleImp.add_visible_discount("s1","computers","D1",0.5,12122104,"pc");
+        storeRoleImp.add_visible_discount("s1","computers","D2",0.5,12122104,"apple");
+        String[] array = new String[2];
+        array[0] = "D1";
+        array[1] = "D2";
+        storeRoleImp.add_complex_discount("s1","computers","D3",array,"and",12122104);
+        storeRoleImp.delete_discount("s1","computers","D1");
+        storeRoleImp.delete_discount("s1","computers","D2");
+
+        subscriberImp.save_products("s2","pc","computers",1);
+        subscriberImp.save_products("s2","apple","computers",1);
+
+        subscriberImp.buy_products_in_cart("s2","moti","1234567891234567","12/24",123,"123456789","","","","");
+        assertEquals(800, s2.getPurchaseProcesslist().get(3).getShoppingBag().getDiscounted_bag_price(), 0.05);
+
+        subscriberImp.save_products("s2","pc","computers",1);
+        subscriberImp.save_products("s2","orange","computers",1);
+
+        subscriberImp.buy_products_in_cart("s2","moti","1234567891234567","12/24",123,"123456789","","","","");
+
+        assertEquals(1600, s2.getPurchaseProcesslist().get(4).getShoppingBag().getDiscounted_bag_price(), 0.05);
+
+        storeRoleImp.delete_discount("s1","computers","D3");
+
+        storeRoleImp.add_visible_discount("s1","computers","D1",0.5,12122104,"pc");
+        storeRoleImp.add_conditioned_discount("s1","computers","pc","D2",0.5,12122104,0,100);
+        subscriberImp.save_products("s2","pc","computers",1);
+        subscriberImp.save_products("s2","orange","computers",1);
+        subscriberImp.save_products("s2","apple","computers",1);
+
+        subscriberImp.buy_products_in_cart("s2","moti","1234567891234567","12/24",123,"123456789","","","","");
+
+        assertEquals(475, s2.getPurchaseProcesslist().get(5).getShoppingBag().getDiscounted_bag_price(), 0.05);
     }
 
 
